399358c00fde316bc486ae7dae5cd535960c14fe
Dereth <54715844+Dereth@users.noreply.github.com>
2021-02-24 19:44:52 -0500

    Add files via upload
    Added newly made app files

---
+++ app/src/main/java/com/gmail/comcorecrew/comcore/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gmail.comcorecrew.comcore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
