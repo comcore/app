d3ebea58d3eb7f2748ac774536a882258ee701e6
Dereth <dthomson@purdue.edu>
2021-03-03 21:04:55 -0500

    Fixed integer storing bug in Cacher class.

--- app/src/main/java/com/gmail/comcorecrew/comcore/classes/Cacher.java
+++ app/src/main/java/com/gmail/comcorecrew/comcore/classes/Cacher.java
@@ -26,10 +26,14 @@ public class Cacher {
 
             //Adds number of lines to file, and for each cacheable, adds the length
             //of the data to the file and then the string of chars.
-            pw.print(data.size());
+            int size = data.size();
+            pw.print((char) (size >> 16));
+            pw.print((char) size);
             for (cacheable group : data) {
                 char[] line = group.toCache();
-                pw.print(line.length);
+                int lineLength = line.length;
+                pw.print((char) (lineLength >> 16));
+                pw.print((char) lineLength);
                 pw.print(line);
             }
 
@@ -49,11 +53,13 @@ public class Cacher {
             File cacheFile = new File(context.getCacheDir(), filename);
             BufferedReader br = new BufferedReader(new FileReader(cacheFile));
             int size = br.read();
+            size = (size << 16) | br.read();
 
             //Creates array of chars and reads each line into the array.
             char[][] data = new char[size][];
             for (int i = 0; i < size; i++) {
                 int lineSize = br.read();
+                lineSize = (lineSize << 16) | br.read();
                 data[i] = new char[lineSize];
 
                 //If there is an error in the formatting, returns null to indicate an empty cache.
@@ -67,39 +73,5 @@ public class Cacher {
             e.printStackTrace();
             return null;
         }
-    }
-
-    static private String toCharString(int num) {
-        return "" + (char) (num>>16) + (char) num;
-    }
-
-    static private String toCharString(long num) {
-        return "" + (char) (num>>48) + (char) (num>>32) + (char) (num>>16) + (char) num;
-    }
-
-    static private int toInt(String s) {
-        if (s.length() == 0) {
-            return 0;
-        }
-        int val = 0;
-        int chars = Math.min(s.length(), 2);
-        for (int i = 0; i < chars; i++) {
-            val = val << 16;
-            val = val | s.charAt(i);
-        }
-        return val;
-    }
-
-    static private long toLong(String s) {
-        if (s.length() == 0) {
-            return 0;
-        }
-        long val = 0;
-        int chars = Math.min(s.length(), 4);
-        for (int i = 0; i < chars; i++) {
-            val = val << 16;
-            val = val | s.charAt(i);
-        }
-        return val;
     }
 }
