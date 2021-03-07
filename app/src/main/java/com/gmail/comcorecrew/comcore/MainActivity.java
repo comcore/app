aa0f0a57ae1b09c7ab7e0e70fdf2b03ec6da75f8
Madeline04 <bland6@purdue.edu>
2021-03-07 03:19:59 -0500

    Added simple menu bar and color scheme

--- app/src/main/java/com/gmail/comcorecrew/comcore/MainActivity.java
+++ app/src/main/java/com/gmail/comcorecrew/comcore/MainActivity.java
@@ -1,8 +1,11 @@
 package com.gmail.comcorecrew.comcore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.gmail.comcorecrew.comcore.server.ServerConnector;
 import com.gmail.comcorecrew.comcore.server.connection.ServerConnection;
@@ -16,5 +19,30 @@ public class MainActivity extends AppCompatActivity {
         ServerConnector.setConnection(new ServerConnection(this.getBaseContext()));
 
         setContentView(R.layout.activity_main);
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    /** The current menu only shows layouts
+     *  This is for testing purposes only **/
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_main_activity:
+                return true;
+            case R.id.action_create_group:
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
