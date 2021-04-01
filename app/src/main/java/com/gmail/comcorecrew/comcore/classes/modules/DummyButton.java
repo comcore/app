80c6d13b3daa9bb42c58c212385634b995d09221
Dereth <dthomson@purdue.edu>
2021-04-01 17:34:57 -0400

    Added basic dummy module.

--- app/src/main/java/com/gmail/comcorecrew/comcore/classes/modules/DummyButton.java
+++ app/src/main/java/com/gmail/comcorecrew/comcore/classes/modules/DummyButton.java
@@ -4,39 +4,59 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 
+import com.gmail.comcorecrew.comcore.R;
 import com.gmail.comcorecrew.comcore.abstracts.CustomChat;
+import com.gmail.comcorecrew.comcore.caching.CustomItem;
 import com.gmail.comcorecrew.comcore.classes.Group;
 import com.gmail.comcorecrew.comcore.server.ServerConnector;
 import com.gmail.comcorecrew.comcore.server.id.CustomModuleID;
 
-public class DummyButton extends CustomChat {
 
-    boolean pressed;
+/**
+ * Simple dummy class to show that custom modules work.
+ *
+ * If the state is 0, the color is blue.
+ * If the state is 1, the color is red.
+ */
+public class DummyButton extends CustomChat {
 
     public DummyButton(String name, CustomModuleID id, Group group) {
         super(name, id, group);
     }
 
+    public DummyButton(String name, Group group) {
+        super(name, group, "dummy");
+        sendMessage("0");
+    }
+
     @Override
     public void viewInit(@NonNull View view) {
-
     }
 
     @Override
     public int getLayout() {
+        return R.layout.fragment_custom_button;
+    }
+
+    @Override
+    public void refreshView(View view) {
+
+    }
+
+    public int getState() {
+        CustomItem lastItem = getLastItem();
+        if ((lastItem == null) || (lastItem.getData().equals("1"))) {
+            return 1;
+        }
         return 0;
     }
 
-    public DummyButton(String name, Group group) {
-        super(name, group, "dummy");
-        sendMessage("0");
-        pressed = false;
-    }
-
     public int press() {
-        if (pressed) {
-            pressed = false;
+        if (getState() == 0) {
+            sendMessage("1");
+            return 1;
         }
+        sendMessage("0");
         return 0;
     }
 }
