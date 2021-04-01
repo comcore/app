18d39bd6bcd3f5fb0a8ea8d5a7bec1bd9bf1679f
Madeline04 <bland6@purdue.edu>
2021-04-01 16:07:21 -0400

    Connected add, delete, and update tasks

--- app/src/main/java/com/gmail/comcorecrew/comcore/dialogs/ViewMembersDialog.java
+++ app/src/main/java/com/gmail/comcorecrew/comcore/dialogs/ViewMembersDialog.java
@@ -30,7 +30,7 @@ import java.util.ArrayList;
 
 public class ViewMembersDialog extends DialogFragment {
     private RecyclerView recycleView;
-    private MainFragment.CustomAdapter adapter;
+    private CustomAdapter adapter;
     private ArrayList<User> userList;
     private GroupID currentGroup;
 
