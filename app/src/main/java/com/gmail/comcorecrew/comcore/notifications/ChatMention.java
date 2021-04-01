6b9855763c2dde190095ce99ac80bad46214288f
Scott Taylor <taylo840@purdue.edu>
2021-04-01 19:26:18 -0400

    Added mention parsing

--- app/src/main/java/com/gmail/comcorecrew/comcore/notifications/ChatMention.java
+++ app/src/main/java/com/gmail/comcorecrew/comcore/notifications/ChatMention.java
@@ -1,5 +1,7 @@
 package com.gmail.comcorecrew.comcore.notifications;
 
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,12 +17,18 @@ public class ChatMention {
     public final String mentionName;
 
     /**
+     * The index of the mention in the string.
+     */
+    public final int index;
+
+    /**
      * Create a ChatMention from a mentioned name.
      *
      * @param mentionName the name that was mentioned or "all"
      */
-    public ChatMention(String mentionName) {
+    public ChatMention(String mentionName, int index) {
         this.mentionName = mentionName;
+        this.index = index;
     }
 
     /**
@@ -30,7 +38,38 @@ public class ChatMention {
      * @return true if they are mentioned, false otherwise
      */
     public boolean mentionsUser(String name) {
-        return mentionName.equals(name) || mentionName.equals(MENTION_ALL);
+        return mentionName.equals(MENTION_ALL) || mentionName.equals(getName(name, 0));
+    }
+
+    /**
+     * Get the number of characters in the mention.
+     *
+     * @return the number of characters in the mention
+     */
+    public int length() {
+        return mentionName.length() + 1;
+    }
+
+    /**
+     * Extracts a first name from a string. Used for parsing mentions.
+     *
+     * @param message    the message
+     * @param startIndex the start index to parse from
+     * @return
+     */
+    private static String getName(String message, int startIndex) {
+        int endIndex;
+        for (endIndex = startIndex; endIndex < message.length(); endIndex++) {
+            if (Character.isWhitespace(message.charAt(endIndex))) {
+                break;
+            }
+        }
+
+        if (endIndex == startIndex) {
+            return null;
+        } else {
+            return message.substring(startIndex, endIndex);
+        }
     }
 
     /**
@@ -40,6 +79,22 @@ public class ChatMention {
      * @return a list of all chat mentions
      */
     public static List<ChatMention> parseChatMentions(String message) {
-        return new ArrayList<>();
+        ArrayList<ChatMention> mentions = new ArrayList<>(0);
+        int index = 0;
+        while ((index = message.indexOf('@', index)) != -1) {
+            int mentionIndex = index++;
+            String mentionName = getName(message, index);
+            if (mentionName != null) {
+                index += mentionName.length();
+                mentions.add(new ChatMention(mentionName, mentionIndex));
+            }
+        }
+        return mentions;
+    }
+
+    @Override
+    @NonNull
+    public String toString() {
+        return "@" + mentionName;
     }
 }
