79a50cd800b8b7f490e5b4074cb5b8b77e0ec9b0
Dereth <dthomson@purdue.edu>
2021-04-01 21:03:20 -0400

    Tested lockobject to make sure it works.

--- app/src/main/java/com/gmail/comcorecrew/comcore/classes/LockObject.java
+++ app/src/main/java/com/gmail/comcorecrew/comcore/classes/LockObject.java
@@ -2,6 +2,10 @@ package com.gmail.comcorecrew.comcore.classes;
 
 import java.io.Serializable;
 
+/**
+ * Object but serializable. That's all it is. Used to store a final object to an input stream.
+ * Nothing to see here.
+ */
 public class LockObject implements Serializable {
     public LockObject() {}
 }
